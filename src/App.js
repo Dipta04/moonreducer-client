7d796d853fa82776955f9c0d729073813e184b82
Dipta04 <diptakarmakar38@gmail.com>
2023-02-01 07:48:30 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import ProductProvider from './context/ProductProvider';
+import { RouterProvider } from 'react-router-dom';
+import routes from './routes/routes';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductProvider>
+        <RouterProvider router={routes}></RouterProvider>
+      </ProductProvider>
     </div>
   );
 }
---
+++ src/routes/routes.js
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import About from "../pages/About";
+import Cart from "../pages/Cart";
+import Home from "../pages/Home";
+import TopRated from "../pages/TopRated";
+
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/about',
+                element: <About></About>
+            },
+            {
+                path: '/cart',
+                element: <Cart></Cart>
+            },
+            {
+                path: '/top-rated',
+                element: <TopRated></TopRated>
+            }
+        ]
+    }
+])
+
+export default routes;
